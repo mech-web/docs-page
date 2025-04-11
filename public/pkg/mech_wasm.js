4bab971c3b82fd03e83240b478c64d57f842a611
cmontella <cmontella@live.com>
2025-04-11 17:43:46 -0400

    Update mech package

--- public/pkg/mech_wasm.js
+++ public/pkg/mech_wasm.js
@@ -135,7 +135,7 @@ export function main() {
 }
 
 function __wbg_adapter_14(arg0, arg1) {
-    wasm._dyn_core__ops__function__Fn_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__haa2ebdf17ccd1fce(arg0, arg1);
+    wasm._dyn_core__ops__function__Fn_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h096fb76d5914925b(arg0, arg1);
 }
 
 const WasmMechFinalization = (typeof FinalizationRegistry === 'undefined')
@@ -274,7 +274,7 @@ function __wbg_get_imports() {
         const ret = typeof window === 'undefined' ? null : window;
         return isLikeNone(ret) ? 0 : addToExternrefTable0(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper323 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper327 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 102, __wbg_adapter_14);
         return ret;
     };
